f6f5c5c8f4990a7f4f534867e322a7cd06ac6e00
Yudong <sydhhh@gmail.com>
2016-08-11 20:05:29 -0700

    Set up

---
+++ Test/src/my/variable/variableExample.java
@@ -0,0 +1,33 @@
+package my.variable;
+
+public class variableExample {
+	public static void main(String[] args) {
+
+        // Declare a variable of type int (integer 32-bit)
+        int firstNumber;
+
+        // Assigning values to firstNumber
+        firstNumber = 10;
+
+        System.out.println("First Number =" + firstNumber);
+
+        // Declare a 32-bit real number (float)
+        // This number is assigned a value of 10.2
+        float secondNumber = 10.2f;
+
+        System.out.println("Second Number =" + secondNumber);
+
+        // Declare a 64-bit real numbers
+        // This number is assigned a value of 10.2
+        // character d at the end to tell with Java this is the type double.
+        // Distinguished from a float.
+        double thirdNumber = 10.2d;
+
+        System.out.println("Third Number =" + thirdNumber);
+
+        // Declare a character
+        char ch = 'a';
+
+        System.out.println("Char ch= " + ch);
+	}
+}

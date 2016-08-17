565d7d0c1353575d220c0fd0e0b5e32f6ea052d3
Yudong <sydhhh@gmail.com>
2016-08-16 22:40:35 -0700

    Null

---
+++ Test/src/test/aaa.java
@@ -0,0 +1,12 @@
+package test;
+
+import static java.lang.Math.*;
+import static java.lang.System.out;
+
+public class aaa {
+	public static void main(String [] args){  
+		out.println("sqrt " + sqrt(2.0));
+	   }
+	
+	
+}
